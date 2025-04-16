fd26e597c663aceacbe52233dd3ac6337be89d0d
Konrad Kording <koerding@gmail.com>
2025-04-16 09:38:42 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,11 +19,4 @@ root.render(
   </React.StrictMode>
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
 
