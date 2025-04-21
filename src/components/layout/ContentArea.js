dbca7b80e945c61d0df8dd8f3623b97ff1b35385
Konrad Kording <koerding@gmail.com>
2025-04-20 22:06:01 -0400

    Create ContentArea.js

---
+++ src/components/layout/ContentArea.js
@@ -0,0 +1,75 @@
+// FILE: src/components/layout/ContentArea.js
+
+import React from 'react';
+import LeftPanel from './LeftPanel';
+import FullHeightInstructionsPanel from '../rightPanel/FullHeightInstructionsPanel';
+
+/**
+ * Component for the main content area with left and right panels
+ */
+const ContentArea = ({
+  // Left panel props
+  activeSection,
+  userInputs,
+  handleInputChange,
+  localSectionContent,
+  isAnyAiLoading,
+  activeApproach,
+  activeDataMethod,
+  setActiveSectionWithManualFlag,
+  handleApproachToggle,
+  handleDataMethodToggle,
+  sectionRefs,
+  handleEdit,
+  handleSignificantEdit,
+  
+  // Right panel props
+  sectionDataForPanel,
+  handleMagic
+}) => {
+  return (
+    <div style={{ paddingTop: '40px' }}>
+      <div className="flex">
+        {/* Left panel with sections */}
+        <LeftPanel 
+          activeSection={activeSection}
+          userInputs={userInputs}
+          handleInputChange={handleInputChange}
+          localSectionContent={localSectionContent}
+          isAnyAiLoading={isAnyAiLoading}
+          activeApproach={activeApproach}
+          activeDataMethod={activeDataMethod}
+          setActiveSectionWithManualFlag={setActiveSectionWithManualFlag}
+          handleApproachToggle={handleApproachToggle}
+          handleDataMethodToggle={handleDataMethodToggle}
+          sectionRefs={sectionRefs}
+          handleEdit={handleEdit}
+          handleSignificantEdit={handleSignificantEdit}
+        />
+      </div>
+
+      {/* Right panel with instructions */}
+      <FullHeightInstructionsPanel
+        currentSection={sectionDataForPanel}
+        improveInstructions={handleMagic}
+        loading={isAnyAiLoading}
+      />
+
+      {/* Footer */}
+      <div className="text-center text-gray-500 text-sm mt-6 border-t border-gray-200 pt-3 pb-3 bg-white">
+        <p>
+          Scientific Project Planner • Designed with Love for Researchers by Konrad @Kordinglab • {new Date().getFullYear()}
+          <span className="mx-2">•</span>
+          <button 
+            onClick={() => window.dispatchEvent(new CustomEvent('openPrivacyPolicy'))}
+            className="text-blue-500 hover:text-blue-700 underline"
+          >
+            Privacy Policy
+          </button>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ContentArea;
