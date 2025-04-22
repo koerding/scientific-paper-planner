fe2a57a618dbb56caac836132f7dbffb73b5a583
Konrad Kording <koerding@gmail.com>
2025-04-22 17:42:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-// FILE: src/App.js
-
+// src/App.js
 import React, { useEffect } from 'react';
 import PaperPlannerApp from './PaperPlannerApp';
 import { resetAllState } from './services/resetService';
 
+// Import all context providers
+import { ProjectProvider } from './contexts/ProjectContext';
+import { SectionProvider } from './contexts/SectionContext';
+import { UIProvider } from './contexts/UIContext';
+import { ChatProvider } from './contexts/ChatContext';
+import { FeedbackProvider } from './contexts/FeedbackContext';
+
+// Import section data to get the section IDs
+import sectionContent from './data/sectionContent.json';
+
 function App() {
+  // Get all section IDs for the SectionProvider
+  const allSectionIds = sectionContent.sections.map(section => section.id);
+
   // Initialize app and set up any global listeners
   useEffect(() => {
     // Set up a global handler for handling reset commands from anywhere in the app
@@ -21,7 +33,18 @@ function App() {
 
   return (
     <div className="App">
-      <PaperPlannerApp />
+      {/* Wrap the entire app with all context providers */}
+      <ProjectProvider>
+        <SectionProvider sectionIds={allSectionIds}>
+          <FeedbackProvider>
+            <ChatProvider>
+              <UIProvider>
+                <PaperPlannerApp />
+              </UIProvider>
+            </ChatProvider>
+          </FeedbackProvider>
+        </SectionProvider>
+      </ProjectProvider>
     </div>
   );
 }
