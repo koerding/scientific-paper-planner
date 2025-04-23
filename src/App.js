700f02062f321de957c1be3013a1d38f99e854af
Konrad Kording <koerding@gmail.com>
2025-04-23 11:53:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,21 @@
 import React, { useEffect } from 'react';
 import PaperPlannerApp from './components/PaperPlanner/VerticalPaperPlannerApp';
 import useAppStore from './store/appStore';
-// import { UIProvider } from './contexts/UIContext'; // REMOVED
-import { ChatProvider } from './contexts/ChatContext'; // Keep ChatProvider if still used
+// import { ChatProvider } from './contexts/ChatContext'; // REMOVED
 
 function App() {
   useEffect(() => {
     window.resetApp = () => {
       useAppStore.getState().resetState();
     };
-    // Initialize onboarding check after store potentially hydrates
-    // useAppStore.getState()._initializeOnboarding(); // Moved to store onRehydrate
-
     return () => { delete window.resetApp; };
   }, []);
 
   return (
     <div className="App">
-      {/* Keep ChatProvider if chat state is separate */}
-      <ChatProvider>
-         {/* <UIProvider> REMOVED */}
-            <PaperPlannerApp />
-         {/* </UIProvider> REMOVED */}
-      </ChatProvider>
+      {/* <ChatProvider> REMOVED */}
+         <PaperPlannerApp />
+      {/* </ChatProvider> REMOVED */}
     </div>
   );
 }
