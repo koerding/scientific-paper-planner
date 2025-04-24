d803344981a23227eb6c617cde9880abc1b6af6e
Konrad Kording <koerding@gmail.com>
2025-04-24 17:06:15 -0400

    Update AppHeader.js

--- src/components/layout/AppHeader.js
+++ src/components/layout/AppHeader.js
@@ -151,14 +151,14 @@ const AppHeader = ({
                )} Load
                <input type="file" className="hidden" accept=".json" onChange={handleFileSelection} disabled={isAiBusy || localImportLoading} />
             </label>
-            {/* Examples */}
+            {/* Examples 
             <button onClick={() => setShowExamplesDialog(true)} disabled={isAiBusy || localImportLoading} className={getButtonClasses()}>
                {isAiBusy ? loadingSpinner : (
                    <svg className="h-3 w-3 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
                    </svg>
                )} Examples
-            </button>
+            </button> */
             {/* Export */}
             <button onClick={exportProject} disabled={isAiBusy || localImportLoading} className={getButtonClasses()}>
                {isAiBusy ? loadingSpinner : (
