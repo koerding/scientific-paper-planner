86403433c10be8ff47bab3459cb09d83b08678e8
Konrad Kording <koerding@gmail.com>
2025-05-01 12:51:36 -0400

    Delete src/PaperPlannerApp.js

--- src/PaperPlannerApp.js
+++
@@ -1,7 +0,0 @@
-/**
-  * Compatibility bridge for the refactored PaperPlannerApp
-  * This file maintains backward compatibility with existing imports
-  */
- 
- import PaperPlannerApp from './components/PaperPlanner';
- export default PaperPlannerApp;
