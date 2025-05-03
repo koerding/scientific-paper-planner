20d0daac1f9901036a631fc12423b444f39b3ee1
Konrad Kording <koerding@gmail.com>
2025-05-03 11:04:50 -0400

    Update SectionModePicker.js

--- src/components/layout/SectionModePicker.js
+++ src/components/layout/SectionModePicker.js
@@ -30,7 +30,7 @@ const SectionModePicker = ({ currentMode, onModeChange, disabled = false }) => {
         aria-pressed={currentMode === 'write'}
       >
         <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-4 mr-1" fill="none" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+        //  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
         </svg>
         Write
       </button>
@@ -48,7 +48,7 @@ const SectionModePicker = ({ currentMode, onModeChange, disabled = false }) => {
         aria-pressed={currentMode === 'guide'}
       >
         <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-4 mr-1" fill="none" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
+     //     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
         </svg>
         Guide
       </button>
